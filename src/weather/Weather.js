25e0fd94fd237d251d5629c6b4b99be57be1762a
Manuel Sanchez Castro <manuel.sanchez1@ibm.com>
2019-04-24 16:41:33 -0400

    Update styles and template in Weather component

--- src/weather/Weather.js
+++ src/weather/Weather.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Weather.css'
 
 class Weather extends React.Component {
   constructor (props) {
@@ -7,9 +8,12 @@ class Weather extends React.Component {
 
   render () {
     return (
-      <div>
-        <p>Time: {this.props.weather.time}</p>
-        <p>Temperature: {this.props.weather.temperature}</p>
+      <div className="Weather">
+        <h1>{this.props.weather.city}</h1>
+        <small>{this.props.weather.timezone}</small>
+        <h2>{this.props.weather.time}</h2>
+        {this.props.weather.summary}
+        <p>{this.props.weather.temperature} &#8451;</p>
       </div>
     )
   }
