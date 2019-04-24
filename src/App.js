d7f0df633dbb48a9c7770f228cc78e657a3a26d7
Manuel Sanchez Castro <manuel.sanchez1@ibm.com>
2019-04-24 16:44:02 -0400

    Set websocket server URL

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.socket = io('http://localhost:3001');
+    this.socket = io('https://ripley-labs-challenge.herokuapp.com');
     this.socket.on('update', data => {
       this.setState({weather: data});
       console.log(this.state.weather);
@@ -31,13 +31,13 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-main">
-        { this.state.weather.Santiago && <Weather weather={this.state.weather.Santiago} /> }
-        { this.state.weather.Zurich && <Weather weather={this.state.weather.Zurich} /> }
-        { this.state.weather.Auckland && <Weather weather={this.state.weather.Auckland} /> }
-        { this.state.weather.Sydney && <Weather weather={this.state.weather.Sydney} /> }
-        { this.state.weather.Londres && <Weather weather={this.state.weather.Londres} /> }
-        { this.state.weather.Georgia && <Weather weather={this.state.weather.Georgia} /> }
-      </div>
+          { this.state.weather.Santiago && <Weather weather={this.state.weather.Santiago} /> }
+          { this.state.weather.Zurich && <Weather weather={this.state.weather.Zurich} /> }
+          { this.state.weather.Auckland && <Weather weather={this.state.weather.Auckland} /> }
+          { this.state.weather.Sydney && <Weather weather={this.state.weather.Sydney} /> }
+          { this.state.weather.Londres && <Weather weather={this.state.weather.Londres} /> }
+          { this.state.weather.Georgia && <Weather weather={this.state.weather.Georgia} /> }
+        </div>
         
         <Map />
       </div>
